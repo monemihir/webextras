580f2b5c362fa88ed98edaa100ee38ee74ccaa18
monemihir_cp <SND\monemihir_cp>
2013-05-13 00:45:07 -0700

    Adding a bootstrap tooltip helper extension.

--- trunk/WebExtras.Mvc/Bootstrap/BSHtmlHelperExtension.cs
+++ trunk/WebExtras.Mvc/Bootstrap/BSHtmlHelperExtension.cs
@@ -16,7 +16,11 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
 using System.Web.Mvc;
+using System.Linq;
 using WebExtras.Mvc.Html;
 
 namespace WebExtras.Mvc.Bootstrap
@@ -64,5 +68,123 @@ namespace WebExtras.Mvc.Bootstrap
     {
       return new BootstrapProgressBar(type, percent, htmlAttributes);
     }
+
+    #region TooltipFor extensions
+
+    /// <summary>
+    /// Create a Bootstrap tooltip. The tooltip text is retrieved from the
+    /// 'Description' attribute for the property
+    /// </summary>
+    /// <typeparam name="TModel">Type to be scanned</typeparam>
+    /// <typeparam name="TValue">Property to be scanned</typeparam>
+    /// <param name="html">Htmlhelper extension</param>
+    /// <param name="expression">The property lamba expression</param>
+    /// <returns>Bootstrap tooltip attached</returns>
+    public static MvcHtmlString TooltipFor<TModel, TValue>(
+      this HtmlHelper<TModel> html,
+      Expression<Func<TModel, TValue>> expression)
+    {
+      return TooltipFor(html, expression, "right", "hover");
+    }
+
+    /// <summary>
+    /// Create a Bootstrap tooltip
+    /// </summary>
+    /// <typeparam name="TModel">Type to be scanned</typeparam>
+    /// <typeparam name="TValue">Property to be scanned</typeparam>
+    /// <param name="html">Htmlhelper extension</param>
+    /// <param name="expression">The property lamba expression</param>
+    /// <param name="tooltipText">Tooltip text</param>
+    /// <returns>Bootstrap tooltip attached</returns>
+    public static MvcHtmlString TooltipFor<TModel, TValue>(
+      this HtmlHelper<TModel> html,
+      Expression<Func<TModel, TValue>> expression,
+      string tooltipText)
+    {
+      MemberExpression exp = expression.Body as MemberExpression;
+      string tooltip = GetTooltipFor(html, expression);
+
+      return TooltipFor(html, expression, tooltipText, "right", "hover");
+    }
+
+    /// <summary>
+    /// Create a Bootstrap tooltip. The tooltip text is retrieved from the
+    /// 'Description' attribute for the property
+    /// </summary>
+    /// <typeparam name="TModel">Type to be scanned</typeparam>
+    /// <typeparam name="TValue">Property to be scanned</typeparam>
+    /// <param name="html">Htmlhelper extension</param>
+    /// <param name="expression">The property lamba expression</param>
+    /// <param name="placement">[Optional] Placement of the tooltip relative to the help icon</param>
+    /// <param name="trigger">[Optional] Trigger mechanism for the tooltip. Valid values: click, hover, focus</param>
+    /// <returns>Bootstrap tooltip attached</returns>
+    public static MvcHtmlString TooltipFor<TModel, TValue>(
+      this HtmlHelper<TModel> html,
+      Expression<Func<TModel, TValue>> expression,
+      string placement,
+      string trigger)
+    {
+      MemberExpression exp = expression.Body as MemberExpression;
+      string tooltip = GetTooltipFor(html, expression);
+
+      return TooltipFor(html, expression, tooltip, placement, trigger);
+    }
+
+    /// <summary>
+    /// Creates a Bootstrap tooltip
+    /// </summary>
+    /// <typeparam name="TModel">Type to be scanned</typeparam>
+    /// <typeparam name="TValue">Property to be scanned</typeparam>
+    /// <param name="html">Htmlhelper extension</param>
+    /// <param name="expression">The property lamba expression</param>
+    /// <param name="tooltipText">Tooltip text</param>
+    /// <param name="placement">[Optional] Placement of the tooltip relative to the help icon</param>
+    /// <param name="trigger">[Optional] Trigger mechanism for the tooltip. Valid values: click, hover, focus</param>
+    /// <returns>Bootstrap tooltip attached</returns>
+    public static MvcHtmlString TooltipFor<TModel, TValue>(
+      this HtmlHelper<TModel> html,
+      Expression<Func<TModel, TValue>> expression,
+      string tooltipText,
+      string placement,
+      string trigger)
+    {
+      MemberExpression exp = expression.Body as MemberExpression;
+      string fieldId = exp.Member.Name + "_tip";
+
+      TagBuilder i = new TagBuilder("i");
+      i.AddCssClass("icon-info-sign");
+      i.AddCssClass("help-inline");
+      i.Attributes["id"] = fieldId;
+
+      TagBuilder script = new TagBuilder("script");
+      script.Attributes["type"] = "text/javascript";
+      script.InnerHtml = "$('#" + fieldId + "').tooltip({ title: '" + tooltipText + "', placement: '" + placement + "', trigger: '" + trigger + "' });";
+
+      return MvcHtmlString.Create(i.ToString(TagRenderMode.Normal) + script.ToString(TagRenderMode.Normal));
+    }
+
+    /// <summary>
+    /// Get the 'Description' attribute text for the given property
+    /// </summary>
+    /// <typeparam name="TModel">Type to be scanned</typeparam>
+    /// <typeparam name="TValue">Property to be scanned</typeparam>
+    /// <param name="html">Htmlhelper extension</param>
+    /// <param name="expression">The property lambda expression</param>
+    /// <returns>Tooltip text to be displayed</returns>
+    private static string GetTooltipFor<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression)
+    {
+      string tooltip = null;
+      MemberExpression exp = expression.Body as MemberExpression;
+      if (exp != null)
+      {
+        DescriptionAttribute descAtt = exp.Member.GetCustomAttributes(typeof(DescriptionAttribute), false).Cast<DescriptionAttribute>().FirstOrDefault();
+        tooltip = (descAtt == null) ? null : descAtt.Description;
+      }
+      else
+        tooltip = string.Empty;
+      return tooltip;
+    }
+
+    #endregion TooltipFor extensions
   }
 }
