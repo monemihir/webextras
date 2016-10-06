ed8eb0a00412bdc354fc47de10d2a605079d70e9
monemihir <monemihir@hotmail.com>
2016-10-06 18:06:55 +1100

    Adding model state dictionary extensions to ignore errors / retrieve errors from model state.

---
+++ trunk/WebExtras.Mvc/Core/ModelStateDictionaryExtension.cs
@@ -0,0 +1,94 @@
+﻿// 
+// This file is part of - WebExtras
+// Copyright 2016 Mihir Mone
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Specialized;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using WebExtras.Core;
+
+namespace WebExtras.Mvc.Core
+{
+  /// <summary>
+  ///   ModelStateDictionary extension class to add custom/server side validation errors
+  /// </summary>
+  public static class ModelStateDictionaryExtension
+  {
+    /// <summary>
+    ///   ModelStateDictionary extension method which adds custom/server side validation errors for display by MVC
+    /// </summary>
+    /// <param name="modelState">ModelStateDictionary object</param>
+    /// <param name="errors">NameValueCollection of errors: key=property name, value=error message</param>
+    /// <param name="keyPrefix">
+    ///   a string to prefix the final key, used for child objects i.e
+    ///   ProfileViewModel.User.SecretQuestion
+    /// </param>
+    public static void AddModelErrors(
+      this ModelStateDictionary modelState,
+      NameValueCollection errors,
+      string keyPrefix = "")
+    {
+      if (errors == null || errors.Count == 0)
+        return;
+
+      keyPrefix = string.IsNullOrEmpty(keyPrefix) ? "" : keyPrefix + ".";
+
+      foreach (string key in errors.Keys)
+        modelState.AddModelError(keyPrefix + key, errors[key]);
+    }
+
+    /// <summary>
+    ///   Get property errors
+    /// </summary>
+    /// <typeparam name="TModel">Model type</typeparam>
+    /// <param name="modelstate">Current model state dictionary</param>
+    /// <param name="expression">Property selector</param>
+    /// <returns>Property errors if found, else null</returns>
+    /// <exception cref="ArgumentNullException">If expression is null</exception>
+    public static ModelState GetPropertyErrorsFor<TModel>(this ModelStateDictionary modelstate,
+      Expression<Func<TModel>> expression)
+    {
+      if (expression == null)
+        throw new ArgumentNullException("expression");
+
+      string key = WebExtrasUtil.GetFieldNameFromExpression(expression.Body as MemberExpression);
+
+      if (modelstate.ContainsKey(key))
+        return modelstate[key];
+
+      return null;
+    }
+
+    /// <summary>
+    ///   Ignore validation errors for given properties
+    /// </summary>
+    /// <typeparam name="TModel">Model type</typeparam>
+    /// <param name="modelstate">Current model state dictionary</param>
+    /// <param name="expression">Property selectors</param>
+    public static void IgnoreErrorsFor<TModel>(this ModelStateDictionary modelstate,
+      params Expression<Func<TModel, object>>[] expression)
+    {
+      if (expression == null)
+        return;
+
+      if (expression.Length == 0)
+        return;
+
+      Array.ForEach(expression,
+        f => modelstate.Remove(WebExtrasUtil.GetFieldNameFromExpression(f.Body as MemberExpression)));
+    }
+  }
+}
--- trunk/WebExtras.Mvc/Core/ValidationHtmlHelperExtension.cs
+++ trunk/WebExtras.Mvc/Core/ValidationHtmlHelperExtension.cs
@@ -1,59 +1,52 @@
-﻿/*
-* This file is part of - WebExtras
-* Copyright (C) 2014 Mihir Mone
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+﻿// 
+// This file is part of - WebExtras
+// Copyright 2016 Mihir Mone
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Web.Mvc;
+using WebExtras.Core;
 
 namespace WebExtras.Mvc.Core
 {
   /// <summary>
-  /// Validation HTML helper methods
+  ///   Validation HTML helper methods
   /// </summary>
   public static class ValidationHtmlHelperExtension
   {
     #region IsValidFor extensions
 
     /// <summary>
-    /// Gets the validation state of a property
+    ///   Gets the validation state of a property
     /// </summary>
     /// <typeparam name="TModel">Type to be scanned</typeparam>
     /// <typeparam name="TValue">Property to be scanned</typeparam>
     /// <param name="html">Htmlhelper extension</param>
     /// <param name="expression">The property lambda expression</param>
     /// <returns>True if state is valid, else False</returns>
-    public static bool IsValidFor<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression)
+    public static bool IsValidFor<TModel, TValue>(this HtmlHelper<TModel> html,
+      Expression<Func<TModel, TValue>> expression)
     {
       if (expression == null)
         throw new ArgumentNullException("expression");
 
       MemberExpression exp = expression.Body as MemberExpression;
+
+      string mname = WebExtrasUtil.GetFieldNameFromExpression(exp);
+
       bool result = true;
-
-      List<string> buff = exp.ToString().Split('.').ToList();
-      buff.RemoveAt(0);
-      string mname = string.Join(".", buff);
-
-      //if (html.ViewData.ModelState.ContainsKey(exp.Member.Name))
-      //  result = !(html.ViewData.ModelState[exp.Member.Name].Errors.Count > 0);
-
       if (html.ViewData.ModelState.ContainsKey(mname))
         result = !(html.ViewData.ModelState[mname].Errors.Count > 0);
 
@@ -61,7 +54,7 @@ namespace WebExtras.Mvc.Core
     }
 
     /// <summary>
-    /// Get the validation state of a property
+    ///   Get the validation state of a property
     /// </summary>
     /// <param name="html">Htmlhelper extension</param>
     /// <param name="memberName">The member name to be checked</param>
