4914effdbe3d14d8967073ad3303fb1bc8a35436
monemihir <monemihir@hotmail.com>
2016-10-17 14:31:22 +1100

    Adding utility method for enums to be able to retrieve values as strongly types enum.

---
+++ trunk/WebExtras/Core/EnumUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebExtras.Core
+{
+  /// <summary>
+  /// Utility methods for <see cref="Enum"/>
+  /// </summary>
+  public static class EnumUtil
+  {
+    /// <summary>
+    /// Get all values of an enum
+    /// </summary>
+    /// <typeparam name="TEnumType">Enum type</typeparam>
+    /// <returns>A collection of all values of the enum</returns>
+    public static IEnumerable<TEnumType> GetValues<TEnumType>()
+    {
+      if (!typeof(TEnumType).IsEnum)
+        throw new InvalidUsageException("GetValues<> can only be invoked on an enum");
+
+      return Enum.GetValues(typeof(TEnumType)).Cast<TEnumType>();
+    }
+  }
+}
