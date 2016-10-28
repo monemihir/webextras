3e57246b01662fa7d497f2a5d140f7619190f426
monemihir <monemihir@hotmail.com>
2016-10-28 17:07:15 +1100

    Updating assembly info files.

--- trunk/WebExtras.Mvc.tests/Properties/AssemblyInfo.cs
+++ trunk/WebExtras.Mvc.tests/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.66")]
+[assembly: AssemblyFileVersion("2.0.0.66")]
