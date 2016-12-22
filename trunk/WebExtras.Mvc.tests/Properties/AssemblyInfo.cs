1e49c28eaf2698cf66f0e9ade7f8f17679dfe172
monemihir <monemihir@hotmail.com>
2016-12-22 18:05:30 +1100

    Updating assembly version to 2.1.0.15 in preparation for release.

--- trunk/WebExtras.Mvc.tests/Properties/AssemblyInfo.cs
+++ trunk/WebExtras.Mvc.tests/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.0.70")]
-[assembly: AssemblyFileVersion("2.0.0.70")]
+[assembly: AssemblyVersion("2.1.0.15")]
+[assembly: AssemblyFileVersion("2.1.0.15")]
